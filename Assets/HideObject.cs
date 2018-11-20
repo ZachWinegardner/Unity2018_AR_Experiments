bc8578d6ee5d434d3f00adcfe1e0998df0feee6e
Zach Winegardner <winegardner.55@osu.edu>
2018-11-20 17:23:16 -0500

    init commit

---
+++ Assets/HideObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObject : MonoBehaviour {
+
+    void Start()
+    {
+        // get all renderers in this object and its children:
+        var renders = GetComponentsInChildren<Renderer>();
+        foreach (Renderer rendr in renders)
+        {
+            rendr.material.renderQueue = 3000;
+        }
+    }
+	
+}
