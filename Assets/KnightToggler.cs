36adee2f1b8b8b39e3323faa4b09cbf5f9e77cfc
Zach Winegardner <winegardner.55@osu.edu>
2018-11-20 19:32:50 -0500

    Knight Card added, plus skydome

---
+++ Assets/KnightToggler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightToggler : MonoBehaviour {
+
+    public GameObject framedKnight, standingKnight;
+
+    public void ToggleKnight()
+    {
+
+        if (framedKnight.activeSelf)
+        {
+            framedKnight.SetActive(false);
+            standingKnight.SetActive(true); 
+        }
+        else
+        {
+            standingKnight.SetActive(false);
+            framedKnight.SetActive(true); 
+        }
+    }
+}
